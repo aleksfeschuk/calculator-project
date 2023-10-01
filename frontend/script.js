8c59e1b4f3f7b890b8621c657be05f5898b32fa1
aleksfeschuk <https://github.com/aleksfeschuk>
2023-10-01 16:40:01 +0200

    first commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     
     const calcAllButtons = document.querySelectorAll(".all_buttons");
@@ -8,6 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let hasEqualsBeenPressed = false;
     let result = 0;
     let shouldCheckForDecimal = false; 
+    let num1 = 0;
+    let num2 = 0;
+    let action = ''
 
     // Calculating
    
@@ -19,21 +23,28 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (!hasEqualsBeenPressed) {
                     try {
                         result = eval(input.textContent);
-                        displayResult.textContent = isNaN(result) ? "Error" : result;
+                        result = roundNumber(result);
+                        displayResults.textContent = isNaN(result) ? "Error" : result;
                     } catch (error) {
-                        displayResult.textContent = "Error";
+                        displayResults.textContent = "Error";
                     }
                 }
                 hasEqualsBeenPressed = true;
                 break;
             case 'C':
                 input.textContent = '';
-                displayResult.textContent = '';
+                displayResults.textContent = '';
                 result = 0;
                 hasEqualsBeenPressed = false;
                 break;
-            case '/':
-            case '*':
+            case 'AC':
+                input.textContent = '';
+                displayResults.textContent = '';
+                result = 0;
+                hasEqualsBeenPressed = false;
+                break;
+            case '÷':
+            case '×':
             case '-':
             case '+':
             case '%':
@@ -41,13 +52,86 @@ document.addEventListener("DOMContentLoaded", () => {
                 hasEqualsBeenPressed = false;
                 break;
             default: 
-                input.textContent += buttonValue;
-                hasEqualsBeenPressed = false;
+            input.textContent += buttonValue;
+            hasEqualsBeenPressed = false;
                 break;
         }
     }
 
-    calcAllButtons.forEach(button => {
-        button.addEventListener('click', handleButtonClick);
-    });
+    function displayCalculator(toCalc) {
+        const currentInput = input.textContent;
+
+        if(toCalc === 'add') {
+            calc.textContent += '+';
+        } else if(toCalc === 'subtract') {
+            calc.textContent += '-';
+        } else if(toCalc === 'remainder') {
+            calc.textContent += '%';
+        } else if(toCalc === 'multiply') {
+            calc.textContent += '×';
+        } else if(toCalc === 'divide') {
+            calc.textContent += '÷';
+        } else {
+            calc.textContent += `${toCalc}`;
+        }
+        input.textContent = currentInput + toCalc;
+    }
+
+    function roundNumber(calculation) {
+        const rounded = Math.round(calculation * 1000) / 1000;
+        return rounded;
+    }
+
+    const calc = (operation, firstNum, secondNum) => {
+        switch(operation) {
+            case 'clear':
+                return firstNum;
+            case 'divide': 
+                if (secondNum === 0) {
+                    return 'Error: Division by zero';
+                }
+                return firstNum / secondNum;
+            case 'multiply':
+                return firstNum * secondNum;
+            case 'subtract': 
+                return firstNum - secondNum;
+            case 'add':
+                return firstNum + secondNum;
+            case 'remainder':
+                if (secondNum === 0) {
+                    return 'Error: Division by zero';
+                }
+                return firstNum % secondNum;
+            default:
+                return firstNum;
+        }
+    }
+
+    function showCalculation(symbolOperation) {
+        const operationSymbols = {
+            'divide': '÷',
+            'multiply': '×',
+            'subtract': '-',
+            'add': '+',
+            'remainder': '%'
+        };
+
+        if(operationSymbols.hasOwnProperty(symbolOperation)) {
+            const symbol = operationSymbols[symbolOperation];
+            const calculation = calc(action, num1, num2);
+            const calculationResult = `${num1}${symbol}${num2}=${calculation}`;
+            displayResults.textContent = calculationResult;
+        }
+    }
+
+    function setOperatorState(state) {
+        calcAllButtons.forEach((operator) => {
+            operator.disabled = (state === 'disabled' || calcAnswer === 'No' || calcAnswer === Infinity);
+        })
+    }
+
+    calcAllButtons.forEach((button) => {
+        button.addEventListener("click", handleButtonClick);
+      });
+
 });
